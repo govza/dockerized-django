27fcfdb55cabb595cea53eaf376651c5fe43017c
Govza <abuhasub@gmail.com>
2018-10-09 14:39:38 +0200

    Init api app

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'api',
     'users',
 ]
 
