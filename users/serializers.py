56883eac4ee773da4ea3b78287b5b17d92ec4c1c
Govza <abuhasub@gmail.com>
2018-10-09 18:08:38 +0200

    Nested UserProfile serialized

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from . import models
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = '__all__'
+
+
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(many=True, read_only=True)
+
     class Meta:
         model = models.CustomUser
         fields = '__all__'
